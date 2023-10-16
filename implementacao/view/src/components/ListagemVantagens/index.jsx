b6c85db5ae00a0d6efe4ff6f54a888519566b94e
henriquegrissi <hgcsc2003@gmail.com>
2023-10-16 18:17:40 -0300

    Imagem adiicionada

--- implementacao/view/src/components/ListagemVantagens/index.jsx
+++ implementacao/view/src/components/ListagemVantagens/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import './card.css'
+import fotoDeFundo from '../../assets/vantagens.webp';
+
 export const VantagemCard = ({ titulo, descricao, custo, imagemUrl }) => {
   return (
     <div className="vantagem-card">
+      <img src={fotoDeFundo} alt={titulo} />
       <h2>{titulo}</h2>
       <p>{descricao}</p>
       <p>Custo: {custo} créditos</p>
